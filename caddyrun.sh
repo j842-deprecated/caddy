2d968f94df0532ea30290d02121c21c80c5e6bfe
j842 <j@842.be>
2017-02-14 20:20:45 +1300

    ..

--- caddyrun.sh
+++ caddyrun.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
+set -o nounset
+set -e
 
-echo "Wheee!"
+
+function die { echo "$1"; exit 1 ; }
+
+function showhelp {
+   cat <<EOF >&2
+
+caddy
+
+Run reverse proxy.
+Environment variables needed:
+   EMAIL
+   SERVICE_HOST
+   SERVICE_PORT
+   MODE
+   
+
+MODE can be staging or production
+
+EOF
+}
+
+
+[[ -v EMAIL ]] || { showhelp ; die "Missing EMAIL." ; }
+[[ -v SERVICE_HOST ]] || { showhelp ; die "Missing SERVICE_HOST." ; }
+[[ -v SERVICE_PORT ]] || { showhelp ; die "Missing SERVICE_PORT." ; }
+[[ -v MODE ]] || { showhelp ; die "Missing MODE." ; }
+
 
 
 #log stdout
 #errors stdout
+echo "Wheee!"
+
+
+
+
 
